842c81196c73b24a1a3fbfcbd3b1c0c9a867d8db
John Khawam <john.khawam@stitchfix.com>
2026-06-18 18:26:25 -0700

    Update Multi-layer backpropagation (Build a Neural Net 3/5)

--- foundations/multi_layer_backprop.py
+++ foundations/multi_layer_backprop.py
@@ -19,15 +19,15 @@ class Solution:
         #   'db1':   1D list (gradient w.r.t. b1, rounded to 4 decimals)
         #   'dW2':   2D list (gradient w.r.t. W2, rounded to 4 decimals)
         #   'db2':   1D list (gradient w.r.t. b2, rounded to 4 decimals)
-        linear = np.dot(W1, x) + b1
-        a1 = np.maximum(0.0, linear)
+        z1 = np.dot(W1, x) + b1
+        a1 = np.maximum(0.0, z1)
         z2 = np.dot(W2, a1) + b2
         loss = np.mean((z2 - y_true)**2)
         dL_dz2 = 2 * (z2 - y_true)/len(y_true)
         dL_dW2 = np.outer(dL_dz2, a1)
         dL_db2 = dL_dz2
         dL_da1 = np.dot(dL_dz2, W2)
-        dL_dz1 = dL_da1 * (linear > 0)
+        dL_dz1 = dL_da1 * (z1 > 0)
         dL_dW1 = np.outer(dL_dz1, x)
         dL_db1 = dL_dz1
         return {
